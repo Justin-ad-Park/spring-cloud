a54d21d96e3c09b0b702fa108aabb6782e8e6b2d
cjloveu <justin@11street.co.th>
2018-12-26 16:55:15 +0900

    Spring-clound-netflix with file

--- sample-config-service/src/main/java/pl/piomin/service/sample/config/ConfigApplication.java
+++ sample-config-service/src/main/java/pl/piomin/service/sample/config/ConfigApplication.java
@@ -4,6 +4,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+/**
+ * How to run
+ *
+ * 1. Add VM Option
+ * -Dspring.config.name=configserver
+ *
+ * 2. Run ConfigApplication
+ *
+ * 3. Test
+ * JSON : http://localhost:8888/client-service/zone1
+ * Properties : http://localhost:8888/client-service-zone2.properties
+ * YML : http://localhost:8888/client-service-zone3.yml
+ *
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
